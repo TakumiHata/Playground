b2f33a2d301c9686355783ee2af9ad26815c9460
hatakumi323 <takuken135@gmail.com>
2025-01-12 16:28:53 +0900

    ログイン画面

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,22 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', redirectTo: 'server', pathMatch: 'full' },
   {
+    /** ログイン */
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/client/login/login.component').then(
+        (c) => c.LoginComponent
+      ),
+  },
+  {
+    /** ダッシュボード */
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/client/dashboard/home/home.component').then(
+        (c) => c.HomeComponent
+      ),
+  },
+  {
     path: 'server',
     loadComponent: () =>
       import('./pages/server/server.component').then((c) => c.ServerComponent),
