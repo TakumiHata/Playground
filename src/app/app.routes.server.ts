2823c02e9043c619c2c5e39dc8f4ff17ba909af5
hatakumi323 <takuken135@gmail.com>
2025-01-17 01:01:40 +0900

    修正

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,10 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'server',
-    renderMode: RenderMode.Server, // サーバーサイドレンダリング(+ハイドレーション)
-  },
-  {
     path: 'client',
     renderMode: RenderMode.Client, // クライアントサイドレンダリング
   },
